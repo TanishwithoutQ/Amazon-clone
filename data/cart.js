d17c79f25016d48b0ad2321bdbe69b9d28bdacc2
TanishwithoutQ <tanishsingh.243@gmail.com>
2024-12-19 15:16:13 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,19 @@
-export let cart=[{
+export let cart= JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+   cart= [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2,
 },{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 1
 }];
+}
+
+
+function savetolocalstorage(){  
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtocart(productId){
     let matchingcartItem;
@@ -26,6 +35,7 @@ export function addtocart(productId){
         quantity: 1,
       });
     }
+    savetolocalstorage();
   }
 
   export function removefromcart(productId){
@@ -38,6 +48,7 @@ export function addtocart(productId){
       }
     });
     cart=newcart;
+    savetolocalstorage();
   }
 
    
